f8b0b947e6ab236ed3d1317cd506bed41224c3f1
teja <autotesting@gmail.com>
2023-11-21 07:52:00 +0530

    new branch testcase

---
+++ OctEndToEndFramework/src/test/java/modules/Login.java
@@ -0,0 +1,14 @@
+package modules;
+
+import org.testng.annotations.Test;
+
+public class Login {
+
+	
+	
+	@Test
+	public void logintestcase()
+	{
+		System.out.println("new testcase created from development branch");
+	}
+}
